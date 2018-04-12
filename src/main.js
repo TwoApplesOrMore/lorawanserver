d2a389df66dbc2a67cf552263b80c9195aabd55c
twoapplesormore <harryvangastel@hotmail.com>
2018-04-12 15:27:16 +0200

    Implemented /send function

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
+import { error } from "util";
+
 const express = require("express")
 const bodyParser = require("body-parser")
 const fs = require("fs")
+const request = require("request")
 
 const standardMessage = "DBC00001000C0134000000005ACDEE6448656C6C6F210000"
 
+const downlinkurl = "https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/team2lorawan/lorawan2?key=ttn-account-v2.TNR_kCw1V0WyaCjbZT6_n5AHv-hf9PC2KlNDB4d3f_Q"
+
 const app = express()
 app.use(bodyParser.json())
+app.use(express.static("../public"))
 
 app.post("/", (req, res) => {
     fs.appendFile("log.txt", JSON.stringify(req.body), err => {
@@ -14,8 +20,15 @@ app.post("/", (req, res) => {
     res.end("Received the POST")
 })
 
-app.get("/", (req, res) => {
-    res.end("<!DOCTYPE html>\n<html><head><title>FirePy.nl</title></head><body>Welcome to FirePy.nl</body></html>")
+app.post("/send", (req, res) => {
+    request.post(downlinkurl, {"ID" : "007"}, (error, response, body) => {
+        //fap away and do nothing
+    })
 })
+
+// app.get("/", (req, res) => {
+//     res.end("<!DOCTYPE html>\n<html><head><title>FirePy.nl</title></head><body>Welcome to FirePy.nl</body></html>")
+// })
+
 
 app.listen(3000)
