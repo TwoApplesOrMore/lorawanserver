f63167c2d8fdbec0ac208a873227375c4910d602
twoapplesormore <harryvangastel@hotmail.com>
2018-04-12 14:35:25 +0200

    reverted change

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ const standardMessage = "DBC00001000C0134000000005ACDEE6448656C6C6F210000"
 const app = express()
 app.use(bodyParser.json())
 
-app.post("/post", (req, res) => {
+app.post("/", (req, res) => {
     fs.appendFile("log.txt", JSON.stringify(req.body), err => {
         if(err) throw err
     })
