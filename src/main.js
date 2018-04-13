6639022ea27db6d625277f3ec0089fab3f28782c
twoapplesormore <harryvangastel@hotmail.com>
2018-04-13 19:46:19 +0200

    added comments and slight restructure of code

--- src/main.js
+++ src/main.js
@@ -3,14 +3,27 @@ const bodyParser = require("body-parser")
 const fs = require("fs")
 const request = require("request")
 
+// mock base64-compliant message to send back, when testing.
 const standardMessage = "eFBUQnlwM25lRXpVdzdLTXJwOE8="
 
+// URL to reach the LoPy
 const downlinkurl = "https://integrations.thethingsnetwork.org/ttn-eu/api/v2/down/team2lorawan/lorawan2?key=ttn-account-v2.TNR_kCw1V0WyaCjbZT6_n5AHv-hf9PC2KlNDB4d3f_Q"
 
+// standard TTN response, for the mock.
+const response = {
+    "dev_id": "lopy",
+    "port": 1,
+    "confirmed": false,
+    "payload_raw": standardMessage
+}
+
+// setup our web application with express
 const app = express()
 app.use(bodyParser.json())
 app.use(express.static("../public"))
 
+// Method to send the message received from LoPy to other countries
+// TODO: Implement broker. For now, log it into a file
 app.post("/", (req, res) => {
     fs.appendFile("log.txt", JSON.stringify(req.body), err => {
         if(err) throw err
@@ -19,16 +32,12 @@ app.post("/", (req, res) => {
     res.end("Received the POST")
 })
 
-const response = {
-    "dev_id": "lopy",
-    "port": 1,
-    "confirmed": false,
-    "payload_raw": standardMessage
-}
-
+// send function to send mock-message to LoPy. Can be changed slightly to send actual messages, 
+// one the broker has been implemented.
 app.post("/send", (req, res) => {
     new Promise((resolve, reject) => {
 
+        // use the request library to send the message to LoPy
         request({
             url: downlinkurl, 
             method: "POST",
@@ -46,9 +55,4 @@ app.post("/send", (req, res) => {
         res.end(JSON.stringify(result))
     })
 })
-
-
-// res.end("<!DOCTYPE html>\n<html><head><title>FirePy.nl</title></head><body>Welcome to FirePy.nl</body></html>")
-
-
 app.listen(3000)
